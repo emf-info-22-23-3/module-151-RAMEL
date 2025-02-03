2759064358be1899f46350576798086f7721ee1b
--replace-all <--replace-all>
2025-02-03 14:02:11 +0100

    ex9 fini

--- src/exercices/exercice9/serveur/serveur.php
+++ src/exercices/exercice9/serveur/serveur.php
@@ -1,17 +1,25 @@
 <?php
-
+session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	if($_POST['action'] == "connect")
 	{
+
 		// doit controller que le password donné (en post) est bien 'emf'
 		// si c'est ok => enregistrement de "emf" dans une variable de session et écrire '<result>true</result>'
 		// si c'est faux => effacer la variable de session et écrire '<result>false</result>'
-		echo '<result>false</result>';
+		if($_POST['password'] == 'emf'){
+			$_SESSION['logged'] = 'emf';
+			echo '<result>true</result>';
+		} else{
+			echo '<result>false</result>';
+		}
 	}
 
 	if($_POST['action'] == "disconnect")
 	{
 		// effacer la variable de session et écrire <result>true</result>
+		unset($_SESSION['logged']);
+		session_destroy();
 		echo '<result>true</result>';
 	}
 }
@@ -22,7 +30,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET'){
 		// tout d'abord contrôler que la variable de session 'logged' contient bien "emf"
 		// si oui => retourner <users><user><name>Victor Legros</name><salaire>9876</salaire></user><user><name>Marinette Lachance</name><salaire>7540</salaire></user><user><name>Gustave Latuile</name><salaire>4369</salaire></user><user><name>Basile Ledisciple</name><salaire>2384</salaire></user></users>
 		// si non => écrire <message>DROITS INSUFFISANTS</message>
-		echo '<message>DROITS INSUFFISANTS</message>';
+		if (isset($_SESSION['logged'])){
+			if($_SESSION['logged'] == 'emf'){
+				echo '<users><user><name>Victor Legros</name><salaire>9876</salaire></user><user><name>Marinette Lachance</name><salaire>7540</salaire></user><user><name>Gustave Latuile</name><salaire>4369</salaire></user><user><name>Basile Ledisciple</name><salaire>2384</salaire></user></users>';
+			} else {
+				echo '<message>DROITS INSUFFISANTS</message>';
+			}
+		} else {
+			echo '<message>DROITS INSUFFISANTS</message>';
+		}
 	}
 }
 
